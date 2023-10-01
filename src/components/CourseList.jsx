67a0148ccd9faf1816b7516e5aedbb77417eb5ff
Neel Keswani <neel@dhcp-10-105-168-221.wireless.northwestern.private>
2023-10-01 16:48:45 -0500

    Selecting courses

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import '../App.css';
 
-const CourseList = ({ courses, selectedTerm }) => (
-  <div className="grid-container">
-    {Object.entries(courses).map(([id, { term, number, title, meets }]) => {
-      if (term === selectedTerm) {
-        return (
-          <div key={id} className="course-card">
-            <div className="course-title-section">
-              <div className="course-title">{`${term} CS ${number}`}</div>
-              <div>{title}</div>
+const CourseList = ({ courses, selectedTerm, selectedCourses, setSelectedCourses }) => {
+  
+  const toggleCourse = (id) => {
+    const newSelectedCourses = new Set(selectedCourses);
+    if (newSelectedCourses.has(id)) {
+      newSelectedCourses.delete(id);
+    } else {
+      newSelectedCourses.add(id);
+    }
+    setSelectedCourses(newSelectedCourses);
+  };
+
+  return (
+    <div className="grid-container">
+      {Object.entries(courses).map(([id, { term, number, title, meets }]) => {
+        const isSelected = selectedCourses.has(id);
+        const cardStyle = isSelected ? "course-card-selected" : "course-card";
+        
+        if (term === selectedTerm) {
+          return (
+            <div
+              key={id}
+              className={cardStyle}
+              onClick={() => toggleCourse(id)}
+            >
+              <div className="course-title-section">
+                <div className="course-title">{`${term} CS ${number}`}</div>
+                <div>{title}</div>
+              </div>
+              <hr />
+              <div className="course-meets">{meets}</div>
             </div>
-            <hr />
-            <div className="course-meets">{meets}</div>
-          </div>
-        );
-      }
-      return null;
-    })}
-  </div>
-);
+          );
+        }
+        return null;
+      })}
+    </div>
+  );
+};
 
 export default CourseList;
--- src/components/TermPage.jsx
+++ src/components/TermPage.jsx
@@ -4,11 +4,17 @@ import CourseList from './CourseList';
 
 const TermPage = ({ courses }) => {
   const [selectedTerm, setTerm] = useState("Fall");
+  const [selectedCourses, setSelectedCourses] = useState(new Set());
 
   return (
     <div>
       <TermSelector setTerm={setTerm} />
-      <CourseList courses={courses} selectedTerm={selectedTerm} />
+      <CourseList
+        courses={courses}
+        selectedTerm={selectedTerm}
+        selectedCourses={selectedCourses}
+        setSelectedCourses={setSelectedCourses}
+      />
     </div>
   );
 };
